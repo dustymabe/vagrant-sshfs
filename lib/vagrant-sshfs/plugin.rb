f0dbca9406394fc0aa846f5fabb7822a9984df43
Dusty Mabe <dusty@dustymabe.com>
2016-07-27 16:10:38 -0400

    caps: make guests subdir for capabilities
    This will enable us to make host capabilities in a different subdir.

--- lib/vagrant-sshfs/plugin.rb
+++ lib/vagrant-sshfs/plugin.rb
@@ -21,67 +21,67 @@ module VagrantPlugins
       end
 
       guest_capability("linux", "sshfs_mount_folder") do
-        require_relative "cap/linux/sshfs_mount"
+        require_relative "cap/guest/linux/sshfs_mount"
         VagrantPlugins::GuestLinux::Cap::MountSSHFS
       end
 
       guest_capability("linux", "sshfs_unmount_folder") do
-        require_relative "cap/linux/sshfs_mount"
+        require_relative "cap/guest/linux/sshfs_mount"
         VagrantPlugins::GuestLinux::Cap::MountSSHFS
       end
 
       guest_capability("linux", "sshfs_is_folder_mounted") do
-        require_relative "cap/linux/sshfs_mount"
+        require_relative "cap/guest/linux/sshfs_mount"
         VagrantPlugins::GuestLinux::Cap::MountSSHFS
       end
 
       guest_capability("redhat", "sshfs_installed") do
-        require_relative "cap/redhat/sshfs_client"
+        require_relative "cap/guest/redhat/sshfs_client"
         VagrantPlugins::GuestRedHat::Cap::SSHFSClient
       end
 
       guest_capability("redhat", "sshfs_install") do
-        require_relative "cap/redhat/sshfs_client"
+        require_relative "cap/guest/redhat/sshfs_client"
         VagrantPlugins::GuestRedHat::Cap::SSHFSClient
       end
 
       guest_capability("fedora", "sshfs_installed") do
-        require_relative "cap/fedora/sshfs_client"
+        require_relative "cap/guest/fedora/sshfs_client"
         VagrantPlugins::GuestFedora::Cap::SSHFSClient
       end
 
       guest_capability("fedora", "sshfs_install") do
-        require_relative "cap/fedora/sshfs_client"
+        require_relative "cap/guest/fedora/sshfs_client"
         VagrantPlugins::GuestFedora::Cap::SSHFSClient
       end
 
       guest_capability("debian", "sshfs_installed") do
-        require_relative "cap/debian/sshfs_client"
+        require_relative "cap/guest/debian/sshfs_client"
         VagrantPlugins::GuestDebian::Cap::SSHFSClient
       end
 
       guest_capability("debian", "sshfs_install") do
-        require_relative "cap/debian/sshfs_client"
+        require_relative "cap/guest/debian/sshfs_client"
         VagrantPlugins::GuestDebian::Cap::SSHFSClient
       end
 
       guest_capability("arch", "sshfs_installed") do
-        require_relative "cap/arch/sshfs_client"
+        require_relative "cap/guest/arch/sshfs_client"
         VagrantPlugins::GuestArch::Cap::SSHFSClient
       end
 
       guest_capability("arch", "sshfs_install") do
-        require_relative "cap/arch/sshfs_client"
+        require_relative "cap/guest/arch/sshfs_client"
         VagrantPlugins::GuestArch::Cap::SSHFSClient
       end
 
       guest_capability("suse", "sshfs_installed") do
-        require_relative "cap/suse/sshfs_client"
+        require_relative "cap/guest/suse/sshfs_client"
         VagrantPlugins::GuestSUSE::Cap::SSHFSClient
       end
 
       guest_capability("suse", "sshfs_install") do
-        require_relative "cap/suse/sshfs_client"
+        require_relative "cap/guest/suse/sshfs_client"
         VagrantPlugins::GuestSUSE::Cap::SSHFSClient
       end
 
