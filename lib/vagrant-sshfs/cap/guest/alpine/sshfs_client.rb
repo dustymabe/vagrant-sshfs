93ee74677769b49614531ddd4a04b036d254ae2f
Dusty Mabe <dusty@dustymabe.com>
2019-12-11 15:57:47 -0500

    guest: make alpine fuse module loading match what we do for freebsd
    I'd like to keep the approach consistent.

--- lib/vagrant-sshfs/cap/guest/alpine/sshfs_client.rb
+++ lib/vagrant-sshfs/cap/guest/alpine/sshfs_client.rb
@@ -5,14 +5,18 @@ module VagrantPlugins
         def self.sshfs_install(machine)
           # Install sshfs
           machine.communicate.sudo("apk add sshfs")
-
-          # Load the fuse module and autoload it in the feature
+          # Load the fuse module
           machine.communicate.sudo("modprobe fuse")
-          machine.communicate.sudo("echo fuse >> /etc/modules")
         end
 
         def self.sshfs_installed(machine)
-          machine.communicate.test("apk -e info sshfs")
+          installed = machine.communicate.test("apk -e info sshfs")
+          if installed
+              # fuse may not get loaded at boot, so check if it's loaded otherwise force load it
+              machine.communicate.sudo("lsmod | grep fuse || modprobe fuse")
+          end
+
+          installed
         end
       end
     end
