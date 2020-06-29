3bc01b5056e0c77800dae263eed62df390c53dc9
Dusty Mabe <dusty@dustymabe.com>
2020-06-28 23:47:27 -0400

    guest: centos: add a few comments into sshfs_client.rb
    Gain comment parity with what I was doing in the redhat file.

--- lib/vagrant-sshfs/cap/guest/centos/sshfs_client.rb
+++ lib/vagrant-sshfs/cap/guest/centos/sshfs_client.rb
@@ -6,8 +6,11 @@ module VagrantPlugins
 
           case machine.guest.capability("flavor")
             when :centos_8
+              # No need to install epel. fuse-sshfs comes from the PowerTools repo
+              # https://bugzilla.redhat.com/show_bug.cgi?id=1758884
               machine.communicate.sudo("yum -y install --enablerepo=PowerTools fuse-sshfs")
-            when :centos_7, :centos
+            when :centos_7, :centos # centos7 and centos6
+              # Install fuse-sshfs from epel
               if !epel_installed(machine)
                 epel_install(machine)
               end
