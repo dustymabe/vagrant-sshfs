04235d96fb9d3d373a0c83b2e3f47a709f4c38af
Dusty Mabe <dusty@dustymabe.com>
2020-06-29 00:08:04 -0400

    guest: redhat: add best-effort support for RHEL8
    There's really no good way to deal with where to get SSHFS from for
    RHEL8 so just use EPEL7 in a oneshot command for now since the
    fuse-sshfs from EPEL7 seems to work.

--- lib/vagrant-sshfs/cap/guest/redhat/sshfs_client.rb
+++ lib/vagrant-sshfs/cap/guest/redhat/sshfs_client.rb
@@ -3,13 +3,24 @@ module VagrantPlugins
     module Cap
       class SSHFSClient
         def self.sshfs_install(machine)
-          # Install epel rpm if not installed
-          if !epel_installed(machine)
-            epel_install(machine)
-          end
 
-          # Install sshfs (comes from epel repos)
-          machine.communicate.sudo("yum -y install fuse-sshfs")
+          case machine.guest.capability("flavor")
+            when :rhel_8
+              # fuse-sshfs isn't in EPEL8 and how to get it from RHEL repos
+              # without having to have the system subscribed is unclear:
+              # https://github.com/dustymabe/vagrant-sshfs/issues/108#issuecomment-601061947
+              # Using fuse-sshfs from EPEL7 works for now so let's just go with it.
+              # Do the install in such a way that the epel7 repo doesn't hang around
+              # on the system, which may have unintended consequences on RHEL8.
+              machine.communicate.sudo("rpm --import https://dl.fedoraproject.org/pub/epel/RPM-GPG-KEY-EPEL-7")
+              machine.communicate.sudo("yum -y install fuse-sshfs --repofrompath=epel7,'http://download.fedoraproject.org/pub/epel/7/$basearch'")
+            when :rhel_7, :rhel # rhel7 and rhel6
+              # Install fuse-sshfs from epel
+              if !epel_installed(machine)
+                epel_install(machine)
+              end
+              machine.communicate.sudo("yum -y install fuse-sshfs")
+          end
         end
 
         def self.sshfs_installed(machine)
