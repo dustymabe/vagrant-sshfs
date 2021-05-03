b5652a51c42771f45b08a5f603d8546a7717d373
Dusty Mabe <dusty@dustymabe.com>
2021-05-03 12:52:44 -0400

    guest: centos: use lowercase for powertools repo
    It was renamed to `powertools` in 8.3.2011 (and later).
    
    https://wiki.centos.org/action/show/Manuals/ReleaseNotes/CentOS8.2011?action=show&redirect=Manuals%2FReleaseNotes%2FCentOSLinux8#Yum_repo_file_and_repoid_changes
    
    Fixes: https://github.com/dustymabe/vagrant-sshfs/issues/123

--- lib/vagrant-sshfs/cap/guest/centos/sshfs_client.rb
+++ lib/vagrant-sshfs/cap/guest/centos/sshfs_client.rb
@@ -6,9 +6,10 @@ module VagrantPlugins
 
           case machine.guest.capability("flavor")
             when :centos_8
-              # No need to install epel. fuse-sshfs comes from the PowerTools repo
+              # No need to install epel. fuse-sshfs comes from the powertools repo
               # https://bugzilla.redhat.com/show_bug.cgi?id=1758884
-              machine.communicate.sudo("yum -y install --enablerepo=PowerTools fuse-sshfs")
+              # https://github.com/dustymabe/vagrant-sshfs/issues/123
+              machine.communicate.sudo("yum -y install --enablerepo=powertools fuse-sshfs")
             when :centos_7, :centos # centos7 and centos6
               # Install fuse-sshfs from epel
               if !epel_installed(machine)
