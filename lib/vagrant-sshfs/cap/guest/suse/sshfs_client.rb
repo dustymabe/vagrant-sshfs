e493f8a788a27776f421d1e109bdf992ad2e91ca
Dan Čermák <dcermak@suse.com>
2021-07-07 11:36:43 +0200

    Enable sftp subsystem on openSUSE if it is not enabled
    openSUSE Tumbleweed recently stopped enabling the sftp subsystem in sshd which
    breaks using sftp via vagrant-sshfs.
    Thus we now check whether the correct subsystem directive is in
    /etc/ssh/sshd_config and if it missing, then we add it and restart sshd

--- lib/vagrant-sshfs/cap/guest/suse/sshfs_client.rb
+++ lib/vagrant-sshfs/cap/guest/suse/sshfs_client.rb
@@ -4,6 +4,7 @@ module VagrantPlugins
       class SSHFSClient
         def self.sshfs_install(machine)
           machine.communicate.sudo("zypper -n install sshfs")
+          machine.communicate.sudo('if ! grep -q "^[[:space:]]*Subsystem[[:space:]]\+sftp" /etc/ssh/sshd_config; then echo "Subsystem sftp /usr/libexec/ssh/sftp-server" >> /etc/ssh/sshd_config; systemctl restart sshd; fi')
         end
 
         def self.sshfs_installed(machine)
