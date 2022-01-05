ade5564df992870a06d6c9de93909a5e1a7ad812
Even Onsager <even.onsager@gmail.com>
2022-01-05 22:21:54 +0100

    Remove superfluous case/when (only rocky_8 exists)

--- lib/vagrant-sshfs/cap/guest/rocky/sshfs_client.rb
+++ lib/vagrant-sshfs/cap/guest/rocky/sshfs_client.rb
@@ -3,14 +3,10 @@ module VagrantPlugins
     module Cap
       class SSHFSClient
         def self.sshfs_install(machine)
-
-          case machine.guest.capability("flavor")
-            when :rocky_8
-              # No need to install epel. fuse-sshfs comes from the powertools repo
-              # https://bugzilla.redhat.com/show_bug.cgi?id=1758884
-              # https://github.com/dustymabe/vagrant-sshfs/issues/123
-              machine.communicate.sudo("yum -y install --enablerepo=powertools fuse-sshfs")
-          end
+          # No need to install epel. fuse-sshfs comes from the powertools repo
+          # https://bugzilla.redhat.com/show_bug.cgi?id=1758884
+          # https://github.com/dustymabe/vagrant-sshfs/issues/123
+          machine.communicate.sudo("yum -y install --enablerepo=powertools fuse-sshfs")
         end
 
         def self.sshfs_installed(machine)
