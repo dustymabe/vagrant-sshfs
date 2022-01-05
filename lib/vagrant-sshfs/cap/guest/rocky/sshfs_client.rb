ecb8c0336bab4d3605781bc91dddf8b315e0f6a7
Even Onsager <even.onsager@gmail.com>
2022-01-05 21:16:32 +0100

    Add guest capabilities file for Rocky (based on CentOS)

---
+++ lib/vagrant-sshfs/cap/guest/rocky/sshfs_client.rb
@@ -0,0 +1,23 @@
+module VagrantPlugins
+  module GuestRocky
+    module Cap
+      class SSHFSClient
+        def self.sshfs_install(machine)
+
+          case machine.guest.capability("flavor")
+            when :rocky_8
+              # No need to install epel. fuse-sshfs comes from the powertools repo
+              # https://bugzilla.redhat.com/show_bug.cgi?id=1758884
+              # https://github.com/dustymabe/vagrant-sshfs/issues/123
+              machine.communicate.sudo("yum -y install --enablerepo=powertools fuse-sshfs")
+          end
+        end
+
+        def self.sshfs_installed(machine)
+          machine.communicate.test("rpm -q fuse-sshfs")
+        end
+
+      end
+    end
+  end
+end
