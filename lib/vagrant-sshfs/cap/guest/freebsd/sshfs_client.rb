cc887ad6ddd6f99cf7deb0134890253f4129dcb5
Dusty Mabe <dusty@dustymabe.com>
2022-06-20 15:09:12 -0400

    guest: upload fuse module loading for FreeBSD
    Newer FreeBSD calls it "fusefs", not just "fuse".
    
    Fixes https://github.com/dustymabe/vagrant-sshfs/issues/124

--- lib/vagrant-sshfs/cap/guest/freebsd/sshfs_client.rb
+++ lib/vagrant-sshfs/cap/guest/freebsd/sshfs_client.rb
@@ -4,14 +4,22 @@ module VagrantPlugins
       class SSHFSClient
         def self.sshfs_install(machine)
           machine.communicate.sudo("pkg install -y fusefs-sshfs")
-          machine.communicate.sudo("kldload fuse")
+          # older FreeBSD used fuse, newer uses fusefs
+          # https://github.com/dustymabe/vagrant-sshfs/issues/124
+          machine.communicate.sudo("kldload fuse || kldload fusefs")
         end
 
         def self.sshfs_installed(machine)
           installed = machine.communicate.test("pkg info fusefs-sshfs")
           if installed
-              # fuse may not get loaded at boot, so check if it's loaded otherwise force load it
-              machine.communicate.sudo("kldstat -m fuse || kldload fuse")
+              # fuse may not get loaded at boot, so check if it's loaded
+              # If not loaded then force load it
+              loaded = machine.communicate.test("kldstat -m fuse || kldstat -m fusefs")
+              if not loaded
+                # older FreeBSD used fuse, newer uses fusefs
+                # https://github.com/dustymabe/vagrant-sshfs/issues/124
+                machine.communicate.sudo("kldload fuse || kldload fusefs")
+              end
           end
 
           installed
