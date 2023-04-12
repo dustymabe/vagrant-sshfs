3b541577cb0deecb118aec355d2cbff8bdd6e296
Mikhail Novosyolov <m.novosyolov@rosalinux.ru>
2023-04-12 21:54:09 +0300

    Respect RPM provides
    In ROSA Linux sshfs is named as sshfs-fuse, not fuse-sshfs, but provides "sshfs" and "fuse-sshfs":
    https://abf.io/import/sshfs-fuse/blob/4c22a0a852/sshfs-fuse.spec#lc-20
    Do not fail in such cases.

--- lib/vagrant-sshfs/cap/guest/alma/sshfs_client.rb
+++ lib/vagrant-sshfs/cap/guest/alma/sshfs_client.rb
@@ -20,7 +20,7 @@ module VagrantPlugins
         end
 
         def self.sshfs_installed(machine)
-          machine.communicate.test("rpm -q fuse-sshfs")
+          machine.communicate.test("rpm -q --whatprovides fuse-sshfs")
         end
 
         protected
